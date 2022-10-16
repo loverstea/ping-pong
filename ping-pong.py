38a722bc696026ee1a297ff354c0907c4c7710bc
loverstea <115397195+loverstea@users.noreply.github.com>
2022-10-16 13:04:02 +0300

    Update ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -3,18 +3,20 @@ from time import time as timer
 
 
 class GameSprite(sprite.Sprite):
-    def __init__():
+    def __init__(self):
+        pass
+    def reset(self):
         pass
         
 background=transform.scale(image.load("dyhastmish.jpg"),(700,500))
 window = display.set_mode((700, 500))
 class Player(GameSprite):
-    def update(self):
-        keys = key.get_pressed()
-        if keys[K_DOWN] and self.rect.y > 0:
-            self.rect.x = self.rect.y - self.speed
-        if keys[K_UP] and self.rect.y < 490:
-            self.rect.x = self.rect.y + self.speed
+    def update_left:
+        pass
+    def update_right:
+        pass
+            
+playerleft = Player(
 run = True
 clock = time.Clock()
 finish  = False
